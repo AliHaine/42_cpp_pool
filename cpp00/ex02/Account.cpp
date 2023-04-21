dd978f09fa334a19bf851bdce63243c813800555
AliHaine <alitr57120@gmail.com>
2023-04-21 11:25:14 +0200

    cpp00 ex02 UPDATE

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,28 +1,59 @@
-//
-// Created by Ali Yagmur on 4/4/23.
-//
-
 #include "Account.hpp"
 
-static int	Account::getNbAccounts() {
+static int Account::getNbAccounts() {
 	return this->_nbAccounts;
 }
 
-static int	Account::getTotalAmount() {
+static int Account::getTotalAmount() {
 	return this->_totalAmount;
 }
 
-static int	Account::getNbDeposits() {
+static int Account::getNbDeposits() {
 	return this->_totalNbDeposits;
 }
 
-static int	Account::getNbWithdrawals() {
+static int Account::getNbWithdrawals() {
 	return this->_totalNbWithdrawals;
 }
 
-static void	displayAccountsInfos() {
-	std::cout << "Nb accounts:" << getNbDeposits() << std::endl;
-	std::cout << "Total amount:" << getTotalAmount() << std::endl;
-	std::cout << "Total nb deposits:" << Account::getNbAccounts() << std::endl;
-	std::cout << "Total nb withdrawls:" << getNbWithdrawals() << std::endl;
+void Account::displayAccountsInfos() {
+	std::cout << "index:" << Account::getNbAccounts()
+			  << ";total:" << Account::getTotalAmount()
+			  << ";deposits:" << Account::getNbDeposits()
+			  << ";withdrawls:" << Account::getNbWithdrawals() << std::endl;
+}
+
+Account::Account(int initial_deposit) {
+	Account::getTotalAmount() = initial_deposit;
+
+	std::cout << "index:" << Account::getNbAccounts()
+		<< ";amount:" << Account::getTotalAmount()
+		<< ";created" << std::endl;
+
+}
+
+Account::~Account(void) {}
+
+void Account::makeDeposit(int deposit) {
+	Account::getNbDeposits()++;
+	Account::getTotalAmount() += deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+	if (Account::getTotalAmount() - withdrawal < 0)
+		return (false);
+	Account::getNbWithdrawals()++;
+	Account::getTotalAmount() -= withdrawal;
+	return (true);
+}
+
+int	Account::checkAmount(void) {
+	return (Account::getTotalAmount());
+}
+
+static void	Account::displayStatus() {
+	std::cout << "index:" << Account::_accountIndex
+		<< ";total:" << Account::_amount;
+		<< ";deposits:" << Account::_nbDeposits;
+		<< ";withdrawls:" << Account::_nbWithdrawals << std::endl;
 }
